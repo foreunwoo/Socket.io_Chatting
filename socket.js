8b00a5c8484d34a1121aef7f3baa84c069a89c70
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-03-31 00:14:18 +0900

    namespace

--- socket.js
+++ socket.js
@@ -3,29 +3,54 @@ const SocketIO = require('socket.io');
 module.exports = (server) => {
   const io = SocketIO(server, { path: '/socket.io' });
 
-  io.on('connection', (socket) => {
-    const req = socket.request;
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; // 프록시일 때, 그냥일 때
-    console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
-    
-    socket.on('disconnect', () => {
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
-    });
-    socket.on('error', (error) => {
-      console.error(error);
-    });
-    socket.on('message', (data) => {
-      console.log(data);
-    });
-    socket.on('reply', (data) => {
-      console.log(data);
-    });
-    socket.interval = setInterval(() => {
-      socket.emit('news', 'Hello Socket.IO'); // 키, 값
-    }, 3000);
+// 네임스페이스로 실시간 데이터가 전달될 주소를 구별할 수 있다.
+// io.of('/')
+const room = io.of('/room');
+const chat = io.of('/chat');
+
+room.on('connection', (socket) => {
+  console.log('room 네임스페이스에 접속');
+  socket.on('disconnect', () => {
+    console.log('room 네임스페이스 접속 해제');
   });
+});
+
+chat.on('connection', (socket) => {
+  console.log('chat 네임스페이스에 접속');
+  const req = socket.request;
+  const { headers: { referer } } = req;
+  const roomId = referer
+    .split('/')[referer.split('/').length - 1]
+    .replace(/\?.+/, '');
+  // /room/awefadfsf (req.headers.referer)
+  socket.join(roomId); // 방에 접속
+  socket.to(roomId).emit('join', { // 메세지 전송
+    user: 'system',
+    chat: `${req.session.color}님이 입장하셨습니다.`,
+  });
+  socket.on('disconnect', () => {
+    console.log('chat 네임스페이스 접속 해제');
+    socket.leave(roomId); // 방 나가기
+    const currentRoom = socket.adapter.rooms[roomId];
+    const userCount = currentRoom ? currentRoom.length : 0;
+    if (userCount === 0) {
+      axios.delete(`http://localhost:8005/room/${roomId}`)
+        .then(() => {
+          console.log('방 제거 요청 성공');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      socket.to(roomId).emit('exit', {
+        user: 'system',
+        chat: `${req.session.color}님이 퇴장하셨습니다.`,
+      });
+    }
+  });
+});
 };
+  
 
 // 클라이언트 -> http -> 서버
 // 클라이언트 -> ws -> 서버
