21e4054d4bed04c920143961c5a152127908cf2f
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-03-31 00:15:13 +0900

    connect schemas

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
+connect();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
