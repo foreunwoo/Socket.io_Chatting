778f5dae801e8d4fa0dfa7993d8392fdba8a9be1
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-04-01 00:17:05 +0900

    set color-hash

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ColorHash = require('color-hash'); // 익명 사용자를 컬러로 구분하기 위한 패키지
 require('dotenv').config();
 
 const webSocket = require('./socket');
@@ -12,6 +13,16 @@ const connect = require('./schemas');
 
 const app = express();
 connect();
+
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    },
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,17 +33,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false
-    }
-}));
+app.use(sessionMiddleware);
 app.use(flash());
-
+app.use((req, res, next) => {
+    if (!req.session.color) {
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionId); // 세션에 컬러 저장
+    }
+    next();
+});
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
@@ -52,4 +61,4 @@ const server = app.listen(app.get('port'),() => {
     console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app, session); // 웹 소켓 서버와 익스프레스 서버 연결
+webSocket(server, app, sessionMiddleware); // 웹 소켓 서버와 익스프레스 서버 연결
